d354c93bd0cd4d847bd0efd395580d7f03222dfa
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-08-24 14:19:18 +0330

    start project

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import Navbar from "./navbar";
+import Footer from "./footer";
+
+const popins = Poppins({ subsets: ["latin"], weight: "400"});
+
+export const metadata: Metadata = {
+  title: "AZ-blog",
+  description: "Created by Amirhosein Zolfaghari",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={popins.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+  return ( 
+    <div>helo next ! :/</div>
+  );
+}
+ 
+export default HomePage;
